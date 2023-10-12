8f176541eae9325496f7db561d52dcdcdbc2aa2b
Peter Muschick <peter.muschick@capgemini.com>
2023-10-12 11:33:49 +0200

    feat: simplify mapper and remove TimeProvider

--- src/main/java/org/onecx/tenantsvc/domain/mappers/TenantMapMapper.java
+++ src/main/java/org/onecx/tenantsvc/domain/mappers/TenantMapMapper.java
@@ -7,7 +7,6 @@ import jakarta.inject.Inject;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.onecx.tenantsvc.control.services.TimeProvider;
 import org.onecx.tenantsvc.domain.models.TenantMap;
 import org.onecx.tenantsvc.domain.services.TenantMapService;
 
@@ -19,8 +18,6 @@ public abstract class TenantMapMapper {
 
     @Inject
     TenantMapService tenantMapService;
-    @Inject
-    TimeProvider timeProvider;
 
     public abstract TenantMapDTO map(TenantMap tenantMap);
 
@@ -31,23 +28,17 @@ public abstract class TenantMapMapper {
         var tenantMap = new TenantMap();
         tenantMap.setTenantId(tenantMapService.findHighestTenantIdAndAddOne());
         tenantMap.setOrgId(dto.getOrgId());
-        tenantMap.setCreationDate(timeProvider.localDateTimeNow());
-        tenantMap.setModificationCount(0);
         return tenantMap;
     }
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "tenantId", ignore = true)
     @Mapping(target = "creationDate", ignore = true)
     @Mapping(target = "creationUser", ignore = true)
+    @Mapping(target = "modificationDate", ignore = true)
     @Mapping(target = "modificationUser", ignore = true)
     @Mapping(target = "controlTraceabilityManual", ignore = true)
     @Mapping(target = "modificationCount", ignore = true)
     @Mapping(target = "persisted", ignore = true)
-    @Mapping(target = "id", ignore = true)
-    public void update(InputTenantMapDTO tenantMapDTO, @MappingTarget TenantMap tenantMap) {
-
-        tenantMap.setOrgId(tenantMapDTO.getOrgId());
-        tenantMap.setTenantId(tenantMap.getTenantId());
-        tenantMap.setModificationDate(timeProvider.localDateTimeNow());
-        tenantMap.setModificationCount(tenantMap.getModificationCount() + 1);
-    }
+    public abstract void update(InputTenantMapDTO tenantMapDTO, @MappingTarget TenantMap tenantMap);
 }
