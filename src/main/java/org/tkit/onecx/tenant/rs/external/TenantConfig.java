9f5962d0ebd6cd575dfdd25e56cdaf0f519d8f83
Andrej Petras <andrej@ajka-andrej.com>
2024-04-09 15:07:33 +0200

    docs: add documentation (#41)

--- src/main/java/org/tkit/onecx/tenant/rs/external/TenantConfig.java
+++ src/main/java/org/tkit/onecx/tenant/rs/external/TenantConfig.java
@@ -1,18 +1,39 @@
 package org.tkit.onecx.tenant.rs.external;
 
+import io.quarkus.runtime.annotations.ConfigDocFilename;
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
 import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
 import io.smallrye.config.WithName;
 
+/**
+ * Tenant service configuration
+ */
+@ConfigDocFilename("onecx-tenant-svc.adoc")
+@ConfigRoot(phase = ConfigPhase.RUN_TIME)
 @ConfigMapping(prefix = "onecx.tenant")
 public interface TenantConfig {
 
+    /**
+     * Enable or disable default tenant
+     */
     @WithName("default.enabled")
+    @WithDefault("true")
     boolean defaultTenantEnabled();
 
+    /**
+     * Default tenant ID
+     */
     @WithName("default.tenant-id")
+    @WithDefault("default")
     String defaultTenantId();
 
+    /**
+     * Token claim ID of the organization ID
+     */
     @WithName("token.claim.org-id")
+    @WithDefault("orgId")
     String tokenOrgClaim();
 
 }
