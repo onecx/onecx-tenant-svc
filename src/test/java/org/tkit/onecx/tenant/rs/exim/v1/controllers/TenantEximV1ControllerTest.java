76be12592593deb76b6805ff09773eb0244e3745
Andrej Petras <andrej@lorislab.org>
2025-05-21 16:13:37 +0200

    feat: update parent to 1.2.0, update docker images

--- src/test/java/org/tkit/onecx/tenant/rs/exim/v1/controllers/TenantEximV1ControllerTest.java
+++ src/test/java/org/tkit/onecx/tenant/rs/exim/v1/controllers/TenantEximV1ControllerTest.java
@@ -4,7 +4,6 @@ import static io.restassured.RestAssured.given;
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.jboss.resteasy.reactive.RestResponse.Status.*;
-import static org.tkit.quarkus.security.test.SecurityTestUtils.getKeycloakClientToken;
 
 import java.util.Map;
 
